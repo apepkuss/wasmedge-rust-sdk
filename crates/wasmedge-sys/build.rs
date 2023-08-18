47508d5d13a9dd03739f0e697ab72848b03031fc
Xin Liu <sam@secondstate.io>
2023-08-18 07:31:16 +0000

    chore(rust-sys): update build script
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/wasmedge-sys/build.rs
+++ crates/wasmedge-sys/build.rs
@@ -86,12 +86,12 @@ fn main() {
                 debug!("fail to install winget: {:?}", status);
             }
 
-            // set PATH
-            let res =
-                std::env::join_paths([std::path::Path::new("C:\\Program Files\\WasmEdge\\lib")]);
-            if res.is_err() {
-                debug!("fail to append the path to wasmedge to PATH");
-            }
+            // // set PATH
+            // let res =
+            //     std::env::join_paths([std::path::Path::new("C:\\Program Files\\WasmEdge\\lib")]);
+            // if res.is_err() {
+            //     debug!("fail to append the path to wasmedge to PATH");
+            // }
 
             let standalone_dir = std::path::PathBuf::from("C:\\Program Files\\WasmEdge");
             debug!("using standalone extraction at {standalone_dir:?}");
